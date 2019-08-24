f0329d327ddfc4fafd25135874363a3346f21e54
Vezir2701 <54472582+Vezir2701@users.noreply.github.com>
2019-08-24 15:05:20 +0300

    Add files via upload

---
+++ ogrenciler.js
@@ -0,0 +1,33 @@
+{"ogrenciler":[
+	{
+		"ogrenciNo":"1",
+		"grenciAdi":"davut", 
+		"ogrenciSoyad":"öner", 
+		"bolumAdi":"böte",
+		"okulAdi":"çukurova", 
+		"ogrenciSinif":"2"
+		
+	},
+	
+	{
+		"ogrenciNo":"2",
+		"grenciAdi":"davut", 
+		"ogrenciSoyad":"öner", 
+		"bolumAdi":"böte",
+		"okulAdi":"çukurova", 
+		"ogrenciSinif":"3"
+		
+	},
+	{
+		"ogrenciNo":"3",
+		"grenciAdi":"belgin", 
+		"ogrenciSoyad":"öner", 
+		"bolumAdi":"böte",
+		"okulAdi":"çukurova", 
+		"ogrenciSinif":"2"
+		
+	}
+	
+	
+	
+]}
